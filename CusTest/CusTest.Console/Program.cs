93de20a856b2089c98b5e820fe721ec03b64cd83
he_jiangao <he_jiangao@126.com>
2015-10-15 15:03:46 +0800

    这是一个控制台测试项目

---
+++ CusTest/CusTest.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CusTest.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            System.Console.WriteLine("请输入...");
+            input = System.Console.ReadLine();
+            System.Console.WriteLine("您输入的内容是：");
+            System.Console.WriteLine(input);
+            System.Console.ReadKey();
+        }
+    }
+}
