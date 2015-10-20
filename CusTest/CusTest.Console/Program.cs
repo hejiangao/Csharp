29d3b786e652f1607260733de73c6675338de900
he_jiangao <he_jiangao@126.com>
2015-10-20 17:44:56 +0800

    增加了一个类静态成员的测试

--- CusTest/CusTest.Console/Program.cs
+++ CusTest/CusTest.Console/Program.cs
@@ -9,6 +9,15 @@ namespace CusTest.Console
     {
         static void Main(string[] args)
         {
+            //测试一个静态成员
+            //泛型类的静态成员只能在类的一个实例中共享。
+            StaticDemo<string>.x = 4;
+            StaticDemo<int>.x = 5;
+            System.Console.WriteLine("sring:" + StaticDemo<string>.x);
+            System.Console.WriteLine("int:" + StaticDemo<int>.x);
+            StaticDemo<string>.x = 6;
+            System.Console.WriteLine("sring:" + StaticDemo<string>.x);
+            //控制台输入
             string input = string.Empty;
             System.Console.WriteLine("请输入...");
             input = System.Console.ReadLine();
---
+++ CusTest/CusTest.Console/StaticDemo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CusTest.Console
+{
+    public class StaticDemo<T>
+    {
+        public static int x;
+    }
+}
